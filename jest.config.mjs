ea40ad6a03a36f28cebb7f5fe58d1da501720c1a
Gabriel Martins <gabrielmartinsdesouzaa@hotmail.com>
2025-06-07 16:29:28 -0300

    chore: creating .env.test and configuring jest to perform integration tests with the database

--- jest.config.mjs
+++ jest.config.mjs
@@ -7,4 +7,5 @@ export default {
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
   moduleFileExtensions: ["ts", "js", "json", "node"],
+  setupFiles: ["<rootDir>/jest.setup.js"],
 };
